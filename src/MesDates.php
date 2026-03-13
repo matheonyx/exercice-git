08cff634c912d1633c0c4a1c793083df0a56b4a3
Mathéo <133261908+matheonyx@users.noreply.github.com>
2026-03-13 15:02:29 +0100

    Fix: Compatibilité PHP 8.3 et ajout de MesDates.php pour la CI

--- src/MesDates.php
+++ src/MesDates.php
@@ -1,40 +1,12 @@
 <?php
-/**
- * Fichier MesDates pour l'exercice PHPCS
- *
- * PHP version 7.4
- *
- * @category Library
- * @package  UPJV
- * @author   Mathe <mathe@etudiant.upjv.fr>
- * @license  http://opensource.org/licenses/gpl-license.php GNU
- * @link     http://localhost
- */
+namespace App;
 
-namespace UPJV;
-
-use DateTime;
-
-/**
- * Classe MesDates
- *
- * @category Library
- * @package  UPJV
- * @author   Mathe <mathe@etudiant.upjv.fr>
- * @license  http://opensource.org/licenses/gpl-license.php GNU
- * @link     http://localhost
- */
-class MesDates
-{
-    /**
-     * Renvoie la date de demain en JSON
-     *
-     * @return string
-     */
-    public function demain()
-    {
-        $date = new DateTime('tomorrow');
-        $data = array('demain' => $date->format('d-m-Y'));
-        return (string)json_encode($data);
+class Validator {
+    public function getTomorrowDateJson(): string {
+        $tomorrow = new \DateTime('tomorrow');
+        return json_encode([
+            'status' => 'success',
+            'date'   => $tomorrow->format('Y-m-d')
+        ]);
     }
 }
